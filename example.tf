d4b37c492c8a65da072926d30a34506c1cecc5b2
Marshall McDonnell <mcdonnellmt@ornl.gov>
2020-01-31 15:29:13 -0500

    Example and variable config files

---
+++ example.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+    region  = "${var.region}"
+    shared_credentials_file = "/home/ntm/.aws/credentials"
+    profile = "default"
+}
+
+resource "aws_instance" "example" {
+    ami = "${var.amis[var.region]}"
+    instance_type = "t2.micro"
+
+    provisioner "local-exec" {
+        command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+    }
+}
+
+resource "aws_eip" "ip" {
+    vpc = true
+    instance = "${aws_instance.example.id}"
+}
+
+output "ip" {
+    value = "${aws_eip.ip.public_ip}"
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+    default = "us-east-2"
+}
+
+variable "amis" {
+    type = "map"
+    default = {
+        "us-east-1" = "ami-b374d5a5"
+        "us-east-2" = "ami-0d5d9d301c853a04a"
+    }
+}
